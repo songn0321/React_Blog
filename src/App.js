bdca446258d64cefafc9b8f533ba7c2d64e2220f
nan song <songn0321@naver.com>
2021-08-02 17:30:25 +0900

    props사용법

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '자바스크립트 독학']);
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, modal변경] = useState(false);
+  let [타이틀, 타이틀변경] = useState(0);
 
   function 제목변경() {
     var newArray = [...글제목];
@@ -29,7 +30,9 @@ function App() {
         // JSX내에서 for반복문은 사용할 수 없다
         글제목.map(function (i, k) {
           return (<div className="list">
-            <h3>{i}
+            <h3 onClick={() => {
+              타이틀변경(k);
+            }}>{i}
               <span onClick={() => {
                 let copy = [...따봉];
                 copy[k]++;
@@ -41,19 +44,23 @@ function App() {
           </div>)
         })
       }
+      {/* <button onClick={() => { 타이틀변경(0) }}>버튼1</button>
+      <button onClick={() => { 타이틀변경(1) }}>버튼2</button>
+      <button onClick={() => { 타이틀변경(2) }}>버튼3</button> */}
+
       <button onClick={() => { modal === false ? modal변경(true) : modal변경(false) }}>모달창열기</button>
       {
-        modal === true ? <Modal></Modal> : null
+        modal === true ? <Modal 글제목={글제목} 타이틀={타이틀}></Modal> : null
       }
     </div>
   );
 }
 //component 만들기
-function Modal() {
+function Modal(props) {
   return (
     <div>
       <div className="modal">
-        <h2>제목</h2>
+        <h2>{props.글제목[props.타이틀]}</h2>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
